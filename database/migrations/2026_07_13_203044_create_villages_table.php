40952a8faf729c8d57d83ea9c8df1488ff5f5b7d
sameerrashed <sameerrashed101@gmail.com>
2026-07-13 23:39:52 +0300

    اضافة علاقات على الجداول

--- database/migrations/2026_07_13_203044_create_villages_table.php
+++ database/migrations/2026_07_13_203044_create_villages_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('villages', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('city_id')
+                ->constrained('cities')
+                ->cascadeOnDelete();
             $table->timestamps();
         });
     }
